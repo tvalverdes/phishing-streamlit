dcbd279b593ccca2676cec7a81310982361c4de6
Tyrone <73258910+tvalverdes@users.noreply.github.com>
2023-07-14 13:59:48 -0500

    Update pln_svc.py

--- pln_svc.py
+++ pln_svc.py
@@ -39,8 +39,9 @@ def analisis_pln(url):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     from sklearn.metrics import accuracy_score
-    
-    classifiersvm = joblib.load('modelo_entrenadoSVC.pkl')
+    classifiersvm = SVC()
+    classifiersvm.fit(X_train, y_train)
+    #classifiersvm = joblib.load('modelo_entrenadoSVC.pkl')
     y_predsvm = classifiersvm.predict(X_test)
     print(classification_report(y_test, y_predsvm))
     accuracysvm = accuracy_score(y_test, y_predsvm)
